b81bda546568c2624d4fbd49a9af6efd0affcf9d
bossclement <bosstclement@gmail.com>
2024-07-25 15:26:47 +0200

    New changes

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -1,4 +1,4 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
 
 const room = new ClassRoom(100);
-console.log(room._maxStudentsSize === 100)
+console.log(room._maxStudentsSize === 100);
