cdb08998fa196e9dcda35ee3401d8ce6d9745d2b
bossclement <bosstclement@gmail.com>
2024-07-26 16:04:09 +0200

    New changes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  await Promise.all([uploadPhoto(), createUser()])
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
