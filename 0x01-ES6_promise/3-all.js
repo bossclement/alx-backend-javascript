d18c6d1a9e210514918f24102d058796254a558b
bossclement <bosstclement@gmail.com>
2024-07-26 16:03:46 +0200

    New changes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,8 @@ export default async function handleProfileSignup() {
   await Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
