41921717ad8a47691c2e4c1957f3652d17eb8f49
bossclement <bosstclement@gmail.com>
2024-07-30 18:55:44 +0200

    New changes

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
 export default function getStudentsByLocation(array, city) {
+  if (!(array instanceof Array) || typeof city !== 'string') {
+    return [];
+  }
   return array.filter((student) => student.city === city);
 }
